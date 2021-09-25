0c21af5d25779125f2b733e7176b8dd3a82f7dfe
walter lacoste <wlacoste@outlook.com>
2021-09-25 15:10:36 -0300

    finalizacion deletes y mejor orden archivos

--- Procedimientos/alta_consecionario.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `alta_consecionario`(IN nombre VARCHAR(45))
-BEGIN
-if consecionariosRepetidos(nombre) = 0 THEN
-	insert into consecionaria(Nombre, Eliminado) VALUES
-    (nombre, 0);
-    end if;
-
-
-END
--- Procedimientos/update_consecionario.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `update_consecionario`(id INT ,nombreNuevo VARCHAR(45))
-BEGIN
-if consecionariosRepetidos(nombreNuevo) = 0 THEN
-	update consecionaria
-    set Nombre = nombreNuevo
-    where Id = id;
-END IF;
-
-END
